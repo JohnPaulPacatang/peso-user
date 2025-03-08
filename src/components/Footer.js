526c66b4ef69d2e7dafa51e24473b5af5fd535ed
JohnPaulPacatang <johnpaulpacatang1gmail.com>
2025-03-08 14:45:51 +0800

    paa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  
   return (
-    <footer className="bg-gray-300 py-5 mt-auto"> 
-    {/* PAA */}
-      <div className="mx-auto px-6 lg:px-12">
-        <div className="text-center text-black   text-sm">
-          Copyright © 2024 All Rights Reserved by The Researchers
+    <footer className="bg-gray-300 text-black py-6 mt-auto">
+      <div className="container mx-auto px-6 ">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="mb-4 md:mb-0">
+            <p className="font-medium text-base">Prime John Clara</p>
+          </div>
+          
+          <div className="text-sm text-black">
+            <p>Copyright © {currentYear} All Rights Reserved</p>
+          </div>
         </div>
       </div>
     </footer>
